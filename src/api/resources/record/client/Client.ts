c90a03a20b1f0a14be88bba30f5b899c1150a5ac
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-07-12 19:18:58 +0000

    SDK regeneration

--- src/api/resources/record/client/Client.ts
+++ src/api/resources/record/client/Client.ts
@@ -67,7 +67,7 @@ export class Record_ {
                 Authorization: await this._getAuthorizationHeader(),
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@sayari/sdk",
-                "X-Fern-SDK-Version": "0.1.3",
+                "X-Fern-SDK-Version": "0.1.4",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
             },
--- src/api/resources/supplyChain/client/Client.ts
+++ src/api/resources/supplyChain/client/Client.ts
@@ -124,7 +124,7 @@ export class SupplyChain {
                 Authorization: await this._getAuthorizationHeader(),
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@sayari/sdk",
-                "X-Fern-SDK-Version": "0.1.3",
+                "X-Fern-SDK-Version": "0.1.4",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
             },
