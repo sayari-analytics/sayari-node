0c606a2be6b4e7f2c50ac39acf8fceb6f1e4815a
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-10-15 14:15:43 +0000

    SDK regeneration

--- src/api/resources/generatedTypes/types/Entities.ts
+++ src/api/resources/generatedTypes/types/Entities.ts
@@ -7,53 +7,53 @@
  */
 export type Entities =
     /**
-     * A shipment between two entities */
-    | "shipment"
-    /**
-     * An unknown placeholder entity. Rarely used. An unknown entity has insufficient information to be grouped by an existing entity type. */
-    | "unknown"
-    /**
-     * An airplane, helicopter, or other vehicle that travels by flight */
-    | "aircraft"
-    /**
-     * A cargo ship, oil tanker, fishing trawler, or other type of watercraft */
-    | "vessel"
-    /**
      * A tradable financial asset */
     | "security"
-    /**
-     * A discretely registered name used by a person or company not operating under its legal name. This includes doing-business-as (DBA) names, fictitious names, etc. in jurisdictions that treat them as registered objects distinct from the person/company using them. */
-    | "tradename"
-    /**
-     * A civil or criminal legal case or similar type of proceeding */
-    | "legal_matter"
     /**
      * A legal entity or organization */
     | "company"
     /**
-     * A generic placeholder entity. Rarely used. A generic entity typically does not fit any other entity type. */
-    | "generic"
+     * A cargo ship, oil tanker, fishing trawler, or other type of watercraft */
+    | "vessel"
     /**
-     * Land, real estate, real property, or personal property not categorized under another entity type */
-    | "property"
+     * An unknown placeholder entity. Rarely used. An unknown entity has insufficient information to be grouped by an existing entity type. */
+    | "unknown"
+    /**
+     * A natural person (human being) */
+    | "person"
     /**
      * A trademark, patent, copyright, or similar type of intangible property */
     | "intellectual_property"
     /**
-     * A natural person (human being) */
-    | "person";
+     * A discretely registered name used by a person or company not operating under its legal name. This includes doing-business-as (DBA) names, fictitious names, etc. in jurisdictions that treat them as registered objects distinct from the person/company using them. */
+    | "tradename"
+    /**
+     * A generic placeholder entity. Rarely used. A generic entity typically does not fit any other entity type. */
+    | "generic"
+    /**
+     * A civil or criminal legal case or similar type of proceeding */
+    | "legal_matter"
+    /**
+     * Land, real estate, real property, or personal property not categorized under another entity type */
+    | "property"
+    /**
+     * An airplane, helicopter, or other vehicle that travels by flight */
+    | "aircraft"
+    /**
+     * A shipment between two entities */
+    | "shipment";
 
 export const Entities = {
-    Shipment: "shipment",
-    Unknown: "unknown",
-    Aircraft: "aircraft",
-    Vessel: "vessel",
     Security: "security",
-    Tradename: "tradename",
-    LegalMatter: "legal_matter",
     Company: "company",
-    Generic: "generic",
-    Property: "property",
-    IntellectualProperty: "intellectual_property",
+    Vessel: "vessel",
+    Unknown: "unknown",
     Person: "person",
+    IntellectualProperty: "intellectual_property",
+    Tradename: "tradename",
+    Generic: "generic",
+    LegalMatter: "legal_matter",
+    Property: "property",
+    Aircraft: "aircraft",
+    Shipment: "shipment",
 } as const;
