9a7e3bd6cd62ad301464f1984047e4d9727eb6d1
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-12-03 12:03:26 +0000

    SDK regeneration

--- src/serialization/resources/info/types/UsageInfo.ts
+++ src/serialization/resources/info/types/UsageInfo.ts
@@ -9,24 +9,26 @@ import * as core from "../../../../core";
 export const UsageInfo: core.serialization.ObjectSchema<serializers.UsageInfo.Raw, Sayari.UsageInfo> =
     core.serialization.object({
         entity: core.serialization.number().optional(),
-        entitySummary: core.serialization.property("entity_summary", core.serialization.number().optional()),
         record: core.serialization.number().optional(),
         resolve: core.serialization.number().optional(),
+        search: core.serialization.number().optional(),
         searchEntities: core.serialization.property("search_entities", core.serialization.number().optional()),
         searchRecords: core.serialization.property("search_records", core.serialization.number().optional()),
         searchTrade: core.serialization.property("search_trade", core.serialization.number().optional()),
+        tradeTraversal: core.serialization.property("trade_traversal", core.serialization.number().optional()),
         traversal: core.serialization.number().optional(),
     });
 
 export declare namespace UsageInfo {
     interface Raw {
         entity?: number | null;
-        entity_summary?: number | null;
         record?: number | null;
         resolve?: number | null;
+        search?: number | null;
         search_entities?: number | null;
         search_records?: number | null;
         search_trade?: number | null;
+        trade_traversal?: number | null;
         traversal?: number | null;
     }
 }
